7d687cb5104980441a125d726d6ffc5089f809f8
telminov <telminov@soft-way.biz>
2023-09-17 16:21:53 +0300

    расширил представление кодеров

--- core/serializers.py
+++ core/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from core import models
 
 
+class UnitShort(serializers.ModelSerializer):
+    class Meta:
+        model = models.Unit
+        fields = ('id', 'name')
+
+
 class Coder(serializers.ModelSerializer):
     score = serializers.SerializerMethodField()
+    units = UnitShort(many=True)
 
     class Meta:
         model = models.Coder
